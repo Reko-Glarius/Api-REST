976d903f95318efcd714ac550a34c975fa7de8d5
Ricardo Aliste G <39688076+Reko-Glarius@users.noreply.github.com>
2020-07-09 08:58:26 -0400

    Update API y API_FUNCTION
    Actualizacion de la api y sus funciones, ademas de adicion de comentarios al codigo explicando sus componenete

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from itertools import cycle
 from flask import Flask, escape, request
+from api_function import generar_datos_carreras, generar_ponderaciones_postulante
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -11,12 +12,50 @@ def say_hi():
 @app.route('/topten', methods=['POST'])
 def digito_verificador():
     if(request.method=='POST'):
+        ##############################################  Variables
         nem=request.form.get('nem')
         ranking=request.form.get('ranking')
         matematicas=request.form.get('matematicas')
         lenguaje=request.form.get('lenguajes')
         ciencias=request.form.get('ciencias')
         historia=request.form.get('historia')
+        carreras=generar_datos_carreras() ###Generacion de lista con los datos relevantes de cada carrera
+        mis_carreras=[]
+
+        ##############################################  Calculo de las ponderaciones del estudiante
+        if(ciencias>=historia):
+            ponderaciones_postulante=generar_ponderaciones_postulante(nem, ranking, matematicas, lenguaje, ciencias) ###Generacion ponderaciones para todas las carreras, con ciencias mayor que historia
+        else:
+            ponderaciones_postulante=generar_ponderaciones_postulante(nem, ranking, matematicas, lenguaje, historia) ###Generacion ponderaciones para todas las carreras, con historia mayor que ciencias
+
+        ##############################################  Calculo de la posicion para cada carreras
+        for posicion in range(0,28):
+            contador=-1
+            base=carreras[posicion]
+            mi_numero=ponderacion_postulante[posicion]
+            if(mi_numero>=base[2]):
+                while(mi_numero>=base[2]):
+                    if(mi_numero>base[2]):
+                        contador+=1
+                        mi_numero+=base[3]
+                    else:
+                        mis_carreras.append([posicion+1, contador])
+                        break
+            else:
+                contador=0
+                while(mi_numero<base[2]):
+                    if(mi_numero<base[2]):
+                        base[2]-=base[3]
+                        contador-=1
+                    else:
+                        mis_carreras.append([posicion+1, contador])
+                        break
+        ##############################################  Ordenamiento y re-configuracion listado
+        mis_carreras=ordenar(mis_carreras)
+        for iteracion in range(0,28):
+            datos=mis_carreras[iteracion]
+            if()
+        ##############################################  Creacion del Json final
         return{
             "nem":nem,
             "ranking":ranking
@@ -43,12 +82,7 @@ def digito_verificador():
                 "ingresado": str(n_rut[0]),
                 "correcto": mod
             }
-            """
-        return {
-            "datos": dato
-        }
-    else:
-        return("Metodo utilizado incorrecto; utilizar metodo GET"), 400 
+            """ 
 
 @app.route('/saludo', methods=['POST'])
 def generar_saludo():
--- api_function.py
+++ api_function.py
@@ -1,11 +1,41 @@
-def generar_datos_carreras():
+def generar_datos_carreras(): ###Funcion para generar lista con todos los datos relevantes de todas las carreras
     nom_car=[]
     cod_car=[]
     alfa_car=[]
     omega_car=[]
     cant_car=[]
     L=[]
-    for iteracion in range(0,28):
+    for iteracion in range(0,28): ###Ciclo iterativo para juntar toda la informacion
         puntos_per_posicion=((alfa_car[iteracion]-omega_car[iteracion])/cant_car[iteracion])
         aux=[nom_car[iteracion],cod_car[iteracion],omega_car[iteracion],puntos_per_posicion]
         L.append(aux)
+    return L ###Retorno de la lista resultante
+
+def generar_ponderaciones_postulante(nem, ranking, matematicas, lenguaje, ciencia_historia): ###Funcion para la generacion de lista con todas las ponderaciones posibles
+    L=[]
+    for iteracion in range(0,28):
+        if(i==0): ###Caso para carrera 1
+            ponderacion=float()+float()+float()+float()+float() ###Calculo valor ponderado
+            L.append(ponderacion) ###Adicion valor ponderado a la lista
+        elif(i==1): ###Caso para carrera 2
+            ponderacion=float()+float()+float()+float()+float()
+            L.append(ponderacion)
+    return L ###Retorno de la lista resultante
+
+def ordenar(listado): ###Funcion recursiva para ordenar de mayor a menor los valores de una lista
+    izq=[]
+    cen=[]
+    der=[]
+    if(len(listado)<=1): ###Condicion de termino de la recursiva; cuando la cantidad de elementos de la lista a ordenar es 0 o 1, ya que no habria elementos a ordenar
+        return listado
+    else: ###Escenario recursivo, cuando existen 2 o mas elementos, ya que es posible tener que ordenarlos
+        pivote=listado[0][1]
+        for elemento in listado:
+            if(elemento[1]>pivote):
+                izq.append(elemento)
+            elif(elemento[1]==pivote):
+                cen.append(elemento)
+            else:
+                der.append(elemento)
+    return ordenar(izq)+cen+ordenar(der) 
+    
