ba31a4216f7def5f332453a7f0924ce52247ee66
Ricardo Aliste G <39688076+Reko-Glarius@users.noreply.github.com>
2020-07-11 00:41:52 -0400

    Add files via upload

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ def say_hi():
     return 'API funcionando'
 
 @app.route('/topten', methods=['POST']) ###Servicio POST 10mejores: Mediante los puntajes de un postulante, entrega un JSON con las 10 mejores carreras para el
-def 10mejores():
+def generar_top():
     if(request.method=='POST'):
         ##############################################  Variables
         nem=request.form.get('nem')
@@ -52,28 +52,45 @@ def 10mejores():
                     else:
                         mis_carreras.append([posicion+1, contador])
                         break
-        ##############################################  Ordenamiento y re-configuracion listado
+        ##############################################  Ordenamiento de datos y creacion del Json final a retornar
         mis_carreras=ordenar(mis_carreras)
-        for iteracion in range(0,28):
+        Jsons=[]
+        for iteracion in range(0,10):
             datos=mis_carreras[iteracion]
-            if()
-        ##############################################  Creacion del Json final a retornar
-        return{
-            "Carrera 1":str(mis_carreras[0][0])+"%"+str(mis_carreras[0][1])+"%"+str(mis_carreras[0][2])+"%"+str(mis_carreras[0][3]),
-            "Carrera 2":str(mis_carreras[1][0])+"%"+str(mis_carreras[1][1])+"%"+str(mis_carreras[1][2])+"%"+str(mis_carreras[1][3]),
-            "Carrera 3":str(mis_carreras[2][0])+"%"+str(mis_carreras[2][1])+"%"+str(mis_carreras[2][2])+"%"+str(mis_carreras[2][3]),
-            "Carrera 4":str(mis_carreras[3][0])+"%"+str(mis_carreras[3][1])+"%"+str(mis_carreras[3][2])+"%"+str(mis_carreras[3][3]),
-            "Carrera 5":str(mis_carreras[4][0])+"%"+str(mis_carreras[4][1])+"%"+str(mis_carreras[4][2])+"%"+str(mis_carreras[4][3]),
-            "Carrera 6":str(mis_carreras[5][0])+"%"+str(mis_carreras[5][1])+"%"+str(mis_carreras[5][2])+"%"+str(mis_carreras[5][3]),
-            "Carrera 7":str(mis_carreras[6][0])+"%"+str(mis_carreras[6][1])+"%"+str(mis_carreras[6][2])+"%"+str(mis_carreras[6][3]),
-            "Carrera 8":str(mis_carreras[7][0])+"%"+str(mis_carreras[7][1])+"%"+str(mis_carreras[7][2])+"%"+str(mis_carreras[7][3]),
-            "Carrera 9":str(mis_carreras[8][0])+"%"+str(mis_carreras[8][1])+"%"+str(mis_carreras[8][2])+"%"+str(mis_carreras[8][3]),
-            "Carrera 10":str(mis_carreras[9][0])+"%"+str(mis_carreras[9][1])+"%"+str(mis_carreras[9][2])+"%"+str(mis_carreras[9][3])
+            datos_carrera_particular=carreras[datos[0]]
+            if(datos[0]>=0):
+                json_especifico={
+                    "Codigo":datos_carrera_particular[1],
+                    "Nombre":datos_carrera_particular[0],
+                    "Ponderacion":datos[0],
+                    "Posicion":(130-datos[1])
+                }
+                Jsons.append(json_especifico)
+            else:
+                json_especifico={
+                    "Codigo":datos_carrera_particular[1],
+                    "Nombre":datos_carrera_particular[0],
+                    "Ponderacion":datos[0],
+                    "Posicion":"Lista de Espera: "+str(datos[1])
+                }
+                Jsons.append(json_especifico)
+        ### Retorno del JSON final
+        return {
+            "Carrera 1":Jsons[0],
+            "Carrera 2":Jsons[1],
+            "Carrera 3":Jsons[2],
+            "Carrera 4":Jsons[3],
+            "Carrera 5":Jsons[4],
+            "Carrera 6":Jsons[5],
+            "Carrera 7":Jsons[6],
+            "Carrera 8":Jsons[7],
+            "Carrera 9":Jsons[8],
+            "Carrera 10":Jsons[9],
         }
 
 @app.route('/saludo', methods=['GET'])
 def generar_saludo():
-    if request.method == 'POST':
+    if(request.method=='POST'):
         nom = request.form.get('nombre')
         pat = request.form.get('paterno')
         mat = request.form.get('materno')
--- api_function.py
+++ api_function.py
@@ -16,10 +16,40 @@ def generar_ponderaciones_postulante(nem, ranking, matematicas, lenguaje, cienci
     L=[]
     for iteracion in range(0,28):
         if(i==0): ###Caso para carrera 1
-            ponderacion=float()+float()+float()+float()+float() ###Calculo valor ponderado
+            ponderacion=float(nem*0.15+ranking*0.2+lenguaje*0.3+matematicas*0.25+ciencia_historia*0.1) ###Calculo valor ponderado
             L.append(ponderacion) ###Adicion valor ponderado a la lista
         elif(i==1): ###Caso para carrera 2
-            ponderacion=float()+float()+float()+float()+float()
+            ponderacion=float(nem*0.2+ranking*0.2+lenguaje*0.4+matematicas*0.1+ciencia_historia*0.1)
+            L.append(ponderacion)
+        elif(i==2): ###Caso para carrera 3
+            ponderacion=float(nem*0.2+ranking*0.2+lenguaje*0.3+matematicas*0.15+ciencia_historia*0.15)
+            L.append(ponderacion)
+        elif(i>=3 and i<=6): ###Caso para carreras 4-7
+            ponderacion=float(nem*0.1+ranking*0.2+lenguaje*0.3+matematicas*0.3+ciencia_historia*0.1)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 8
+            ponderacion=float(nem*0.15+ranking*0.25+lenguaje*0.2+matematicas*0.2+ciencia_historia*0.2)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 9-10
+            ponderacion=float(nem*0.2+ranking*0.2+lenguaje*0.15+matematicas*0.35+ciencia_historia*0.1)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 11
+            ponderacion=float(nem*0.15+ranking*0.35+lenguaje*0.2+matematicas*0.2+ciencia_historia*0.1)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 12-13
+            ponderacion=float(nem*0.15+ranking*0.25+lenguaje*0.2+matematicas*0.3+ciencia_historia*0.1)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 14-15
+            ponderacion=float(nem*0.1+ranking*0.25+lenguaje*0.15+matematicas*0.3+ciencia_historia*0.2)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 16-17
+            ponderacion=float(nem*0.1+ranking*0.4+lenguaje*0.3+matematicas*0.1+ciencia_historia*0.1)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 18
+            ponderacion=float(nem*0.2+ranking*0.3+lenguaje*0.2+matematicas*0.1+ciencia_historia*0.2)
+            L.append(ponderacion)
+        elif(i==1): ###Caso para carrera 19-28
+            ponderacion=float(nem*0.1+ranking*0.25+lenguaje*0.2+matematicas*0.35+ciencia_historia*0.1)
             L.append(ponderacion)
     return L ###Retorno de la lista resultante
 
